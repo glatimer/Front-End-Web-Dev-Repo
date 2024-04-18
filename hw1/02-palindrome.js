a3c9c1a604bb9772ec41b18a8467e2ad73f9689f
Genevieve Latimer <genevievelatimer@gmail.com>
2024-04-17 22:38:49 -0700

    Displayed result to HTML. Need to add styling.

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,5 +1,7 @@
+// Declarations
 const elem = document.querySelector("input");
 elem.addEventListener("change", handleInput); // get input on enter
+const output = document.getElementById("result"); // output the result to HTML
 
 // Check for positive number
 function isPositiveNumber(num) {
@@ -12,21 +14,18 @@ function isPositiveNumber(num) {
 function reverseNumber(num) {
   let result = "";
   for (i = String(num).length - 1; i >= 0; --i) {
-    console.log("i: " + num[i]);
     result = result + num[i];
   }
-  console.log("result: ", result);
   return result;
 }
 
 function handleInput() {
   // Element to string
   const userInput = elem.value;
-  console.log("input: " + userInput);
 
   // Check for a positive number
   if (isPositiveNumber(userInput) === false) {
-    console.error("Please, enter a positive number.");
+    window.alert("Please, enter a positive number.");
     return;
   }
 
@@ -35,8 +34,8 @@ function handleInput() {
 
   // Compare the reverse with original for result
   if (userInput === reverse) {
-    console.log("Yes. " + userInput + " is a palindrome.");
+    output.innerHTML = "Yes. " + userInput + " is a palindrome.";
   } else {
-    console.log("No. Try again.");
+    output.innerHTML = "No. Try again.";
   }
 }
