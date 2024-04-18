f7f13ae9352a376ca736c607fb3c50076bd00018
Genevieve Latimer <genevievelatimer@gmail.com>
2024-04-17 22:30:57 -0700

    Completed reverse and compare.  Need to output text to HTML instead of console.

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,12 +1,22 @@
 const elem = document.querySelector("input");
-
-elem.addEventListener("change", handleInput);
+elem.addEventListener("change", handleInput); // get input on enter
 
 // Check for positive number
 function isPositiveNumber(num) {
   if (num < 0) {
     return false;
   }
+}
+
+// Reverse a number
+function reverseNumber(num) {
+  let result = "";
+  for (i = String(num).length - 1; i >= 0; --i) {
+    console.log("i: " + num[i]);
+    result = result + num[i];
+  }
+  console.log("result: ", result);
+  return result;
 }
 
 function handleInput() {
@@ -18,5 +28,15 @@ function handleInput() {
   if (isPositiveNumber(userInput) === false) {
     console.error("Please, enter a positive number.");
     return;
+  }
+
+  // Get the reverse of the userInput
+  const reverse = reverseNumber(userInput);
+
+  // Compare the reverse with original for result
+  if (userInput === reverse) {
+    console.log("Yes. " + userInput + " is a palindrome.");
+  } else {
+    console.log("No. Try again.");
   }
 }
