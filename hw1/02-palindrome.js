3c1036866f0ab8859c42bc5799840e95a2e74365
Genevieve Latimer <genevievelatimer@gmail.com>
2024-04-17 22:15:37 -0700

    completed positive number check.

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,22 @@
-const elem = document.querySelector('input');
+const elem = document.querySelector("input");
 
-elem.addEventListener('input', handleInput);
+elem.addEventListener("change", handleInput);
+
+// Check for positive number
+function isPositiveNumber(num) {
+  if (num < 0) {
+    return false;
+  }
+}
+
+function handleInput() {
+  // Element to string
+  const userInput = elem.value;
+  console.log("input: " + userInput);
+
+  // Check for a positive number
+  if (isPositiveNumber(userInput) === false) {
+    console.error("Please, enter a positive number.");
+    return;
+  }
+}
