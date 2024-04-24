6b80869e23c34638b2b886b985786fed799f1c4d
Genevieve Latimer <glatimer@pdx.edu>
2024-04-24 13:10:17 -0700

    Managed to grab the data from input.

--- hw2/02-search.js
+++ hw2/02-search.js
@@ -1,3 +1,11 @@
-// Add your code here
+// import { characters } from "02-data.js";
+// const userInput = document.getElementsByClassName(".getData");
 
-userInput.addEventListener('click', handleClick);
+const userInput = document.querySelector(".getData");
+userInput.addEventListener("click", handleClick); // grab data on input click
+
+// Search data.js for the character
+function handleClick() {
+  const search = document.getElementById("userInput").value;
+  console.log(search);
+}
