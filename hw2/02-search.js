49fb43a34b29551678f042e1bf744eb3156f7344
Genevieve Latimer <glatimer@pdx.edu>
2024-04-24 15:24:25 -0700

    Outputting the required data from the search onto the DOM successfully.  Need to format.

--- hw2/02-search.js
+++ hw2/02-search.js
@@ -1,16 +1,6 @@
 const userInput = document.querySelector(".getData");
 userInput.addEventListener("click", handleClick); // grab data on input click
 
-// Display all elements in the result array
-function displayAll(data) {
-  // check for null
-  if (!data) {
-    console.error("The array is empty.");
-    return;
-  }
-  data.forEach((element) => console.log(element));
-}
-
 // Search character array for a match and return the results
 function handleClick() {
   const search = document.getElementById("userInput").value.trim();
@@ -24,5 +14,34 @@ function handleClick() {
   console.log(results);
 
   // Output the results
-  displayAll(results);
+  displayCharacterComponents(results);
+}
+
+// Build a character object
+function createCharacterComponent(characterData) {
+  const container = document.createElement("section");
+  let name = document.createElement("div");
+  let birthYear = document.createElement("div");
+
+  name.textContent = characterData.name;
+  birthYear.textContent = `Birth year: ${characterData.birth_year}`;
+
+  container.append(name);
+  container.append(birthYear);
+  return container;
+}
+
+// Display results to DOM
+function displayCharacterComponents(resultsArray) {
+  let app = document.getElementById("results");
+  // clear previous results
+  if (!resultsArray || resultsArray.length === 0) {
+    console.error("The array is empty.");
+    return;
+  }
+  resultsArray.forEach((element) => {
+    console.log(element.name, element.birth_year);
+    const charComponent = createCharacterComponent(element);
+    app.appendChild(charComponent);
+  });
 }
