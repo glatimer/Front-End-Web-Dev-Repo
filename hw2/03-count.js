0b33daadeecd68ad66c017300a7eda29bc791f7e
Genevieve Latimer <glatimer@pdx.edu>
2024-04-24 16:34:39 -0700

    Captured the input word and searchable text.

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -1,3 +1,12 @@
-// Add your code here
+const input = document.querySelector("#userInput");
+input.addEventListener("keydown", handleKeyDown);
 
-input.addEventListener('keydown', handleKeyDown);
+// When enter is pushed, grab the word to highlight
+function handleKeyDown(event) {
+  const word = input.value.trim();
+  console.log(word);
+
+  // Variable containing searchable content
+  const toSearch = document.querySelector(".card-text");
+  console.log(toSearch);
+}
