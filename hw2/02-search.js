d458e526b2d392a5de535735ce8d9a0bab74c9c0
Genevieve Latimer <glatimer@pdx.edu>
2024-04-24 14:39:25 -0700

    Finished filtering the array for a match.  Need to format and output the results.

--- hw2/02-search.js
+++ hw2/02-search.js
@@ -1,11 +1,28 @@
-// import { characters } from "02-data.js";
-// const userInput = document.getElementsByClassName(".getData");
-
 const userInput = document.querySelector(".getData");
 userInput.addEventListener("click", handleClick); // grab data on input click
 
-// Search data.js for the character
+// Display all elements in the result array
+function displayAll(data) {
+  // check for null
+  if (!data) {
+    console.error("The array is empty.");
+    return;
+  }
+  data.forEach((element) => console.log(element));
+}
+
+// Search character array for a match and return the results
 function handleClick() {
-  const search = document.getElementById("userInput").value;
+  const search = document.getElementById("userInput").value.trim();
   console.log(search);
+
+  // Create array of results
+  const results = characters.filter((toFind) =>
+    toFind.name.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+  );
+
+  console.log(results);
+
+  // Output the results
+  displayAll(results);
 }
