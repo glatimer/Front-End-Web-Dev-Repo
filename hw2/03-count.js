8489bf6375dc263884020cbdb323bd10da5645c2
Genevieve Latimer <glatimer@pdx.edu>
2024-04-24 17:20:00 -0700

    Created a highlighted word and replaced all with it. The results are buggy.

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -1,12 +1,31 @@
 const input = document.querySelector("#userInput");
-input.addEventListener("keydown", handleKeyDown);
+input.addEventListener("keydown", handleKeyDown); // grab input on enter
 
-// When enter is pushed, grab the word to highlight
+// Search and replace inputted word globally with highlighted word
 function handleKeyDown(event) {
   const word = input.value.trim();
-  console.log(word);
 
   // Variable containing searchable content
-  const toSearch = document.querySelector(".card-text");
-  console.log(toSearch);
+  const textBody = document.querySelector(".card-text");
+
+  const regex = new RegExp(word, "g"); // g = global search
+
+  // Create a highlighted word to replace the match
+  const highlightedWord = document.createElement("span");
+  highlightedWord.textContent = word;
+  highlightedWord.style.backgroundColor = "yellow";
+
+  // Replace all matching words with the highlightedWord
+  const highlightedText = textBody.textContent.replaceAll(
+    regex,
+    highlightedWord.outerHTML
+  );
+
+  // Update and output
+  textBody.innerHTML = highlightedText;
 }
+
+// Resources
+/*
+ * regex => https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/search
+ */
