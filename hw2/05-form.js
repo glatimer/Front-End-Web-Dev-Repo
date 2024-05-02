93ed4e19989f5d0f0c63c9d2c56646ef1a36f2fd
Genevieve Latimer <glatimer@pdx.edu>
2024-05-01 21:48:20 -0700

    Completed Exercise 05 - Form.  Everything should be functioning now.

--- hw2/05-form.js
+++ hw2/05-form.js
@@ -1,32 +1,37 @@
-const form = document.querySelector("#myForm"); // Get form
-form.addEventListener("submit", handleSubmit); // Get input on submit
-const submitButton = document.querySelector("#trigger-modal"); // get submit button
-const resetButton = document.querySelector("#trigger-reset"); // get reset button
+const form = document.querySelector("#myForm"); // Get form from HTML
+form.addEventListener("submit", handleSubmit); // Extract user input on submit
 
-// Extract form information and output in a modal
-// Function to handle form submission
+// If submit button is clicked -> output modal
+// If reset button is clicked -> clear form
 function handleSubmit(event) {
   event.preventDefault();
+  const submitter = event.submitter;
+  const handler = submitter.id;
 
   // Extract data from the form using FormData object
   const formData = new FormData(form);
 
-  // Display modal back to user
-  displayModal(formData);
+  // Only display the modal if the submit button was clicked
+  if (handler === "trigger-modal") {
+    displayModal(formData);
+    // Clear the form if reset button is clicked
+  } else if (handler === "trigger-reset") {
+    form.reset();
+  }
 
-  // Clear the form
+  // Otherwise, reset when submit is complete
   form.reset();
-
-  return formData;
 }
 
 // Function to display modal with form data
 function displayModal(formData) {
   const modalBody = document.querySelector("#return-content");
+  modalBody.classList.add("container");
 
   // Loop through the FormData array adding formatting to each element
   for (const [key, value] of formData) {
     const item = document.createElement("div");
+    item.style.padding = "0.3em";
     item.textContent += `${key}: ${value}\n`;
     modalBody.appendChild(item);
   }
@@ -34,97 +39,9 @@ function displayModal(formData) {
   myModal.show();
 }
 
-// Build modal
-function buildModal(dataName) {
-  const modal = document.createElement("div");
-  modal.classList.add("modal", "fade");
-  modal.setAttribute("id", "myModal");
-
-  const modalDialog = document.createElement("div");
-  modalDialog.classList.add("modal-dialog");
-
-  const modalContent = document.createElement("div");
-  modalContent.classList.add("modal-content");
-
-  const modalHeader = document.createElement("div");
-  modalHeader.classList.add("modal-header");
-
-  const modalTitle = document.createElement("h1");
-  modalTitle.classList.add("modal-title");
-  modalTitle.textContent = `Modal Title`;
-
-  const btnClose = document.createElement("button");
-  btnClose.classList.add("btn-close");
-  btnClose.setAttribute("type", "button");
-  btnClose.setAttribute("data-bs-dismiss", "modal");
-  btnClose.setAttribute("aria-label", "Close");
-
-  const modalBody = document.createElement("div");
-  modalBody.classList.add("modal-body");
-  modalBody.textContent = `Full Name: ${dataName}`;
-
-  modalHeader.appendChild(modalTitle);
-  modalHeader.appendChild(btnClose);
-
-  modalContent.appendChild(modalHeader);
-  modalContent.appendChild(modalBody);
-
-  modalDialog.appendChild(modalContent);
-  modal.appendChild(modalDialog);
-  return modal;
-}
-
 /* RESOURCES
  * input autocomplete => https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/autocomplete#values
  * textarea => https://mdbootstrap.com/docs/standard/forms/input-fields/
+ * form data object => https://developer.mozilla.org/en-US/docs/Web/API/FormData
+ * submitter property for button click => https://developer.mozilla.org/en-US/docs/Web/API/SubmitEvent/submitter
  */
-
-// // Extract form information
-// const dataName = document.getElementById("name").value.trim();
-// const dataEmail = document.getElementById("email").value.trim();
-// const dataRegistration = document.getElementById("registration").value;
-// const pLang = document.getElementById("pLang").checked;
-// const OS = document.getElementById("os").checked;
-// const fullStack = document.getElementById("full-stack").checked;
-// const additional = document.getElementById("questions").value.trim();
-
-// Return form data as an object
-// return {
-//   dataName,
-//   dataEmail,
-//   dataRegistration,
-//   pLang,
-//   OS,
-//   fullStack,
-//   additional,
-// };
-
-// Submit button event
-// const submitButton = document.querySelector("#trigger-modal");
-// submitButton.addEventListener("click", submitClick);
-
-// Get form data from the submit event and display a confirmation modal
-// function submitClick() {
-//   const getData = handleSubmit();
-//   displayModal(formData);
-// }
-
-// const modalBody = document.querySelector("#return-content");
-// modalBody.textContent = `Full Name: ${dataName}
-// Email: ${dataEmail}
-// Registered: ${dataRegistration}
-// Classes Taken:
-//   Programming Languages: ${pLang}
-//   Operating Systems: ${OS}
-//   Full Stack Web Development: ${fullStack}
-// Other Comments:
-//   ${additional}`;
-
-// const myModal = new bootstrap.Modal(document.querySelector("#return-modal"));
-
-// Reset button event
-// const resetButton = document.querySelector("#trigger-reset");
-// resetButton.addEventListener("click", function (event) {
-//   // Clear form fields
-//   document.getElementById("myForm").reset();
-// });
